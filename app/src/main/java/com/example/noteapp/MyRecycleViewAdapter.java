9087bb67727600f6a9fd08afde79519981896fea
ngonguyenquangtu <20120234@student.hcmus.edu.vn>
2022-11-02 16:57:17 +0700

    Create a new layout for displaying the single day's events using cardView.

--- app/src/main/java/com/example/noteapp/MyRecycleViewAdapter.java
+++ app/src/main/java/com/example/noteapp/MyRecycleViewAdapter.java
@@ -1,4 +1,32 @@
 package com.example.noteapp;
 
-public class MyRecycleViewAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyRecycleViewAdapter extends RecyclerView.Adapter {
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
